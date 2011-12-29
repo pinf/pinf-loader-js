95be3d63f790fabb367a015746c3086c65fe03b6
cadorn <christoph@christophdorn.com>
2011-12-29 09:13:40 -0800

    misc

--- examples/02-PackageLocalDependencies.js
+++ examples/02-PackageLocalDependencies.js
@@ -5,7 +5,7 @@ require.memoize("/package.json", {
 
 require.memoize("/main.js", function(require, exports, module)
 {
-	// A one-way dependency.
+	// One-way dependency.
 	var GREETINGS = require("./greetings");
 
 	exports.main = function(options)
@@ -17,7 +17,7 @@ require.memoize("/main.js", function(require, exports, module)
 // Flat.
 require.memoize("/greetings.js", function(require, exports, module)
 {
-	// A circular dependency.
+	// Circular dependency.
 	var HELLO = require("./words/hello");
 
 	exports.getGreeting = function()
@@ -34,7 +34,7 @@ require.memoize("/greetings.js", function(require, exports, module)
 // Sub path.
 require.memoize("/words/hello.js", function(require, exports, module)
 {
-	// A circular dependency.
+	// Circular dependency.
 	var GREETINGS = require("../greetings");
 	
 	exports.getWord = function()
